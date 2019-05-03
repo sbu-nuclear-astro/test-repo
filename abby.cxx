7a231c0fbdb62dca4b53f8ebe4d63276138dfff3
Alice Harpole <aliceharpole@gmail.com>
2019-05-03 12:53:47 -0400

    this now compiles

--- abby.cxx
+++ abby.cxx
@@ -1,31 +1,5 @@
-yeet
-you know what
-kiran
-fight 
-me 
-i 
-see
-you
-typing
-1
-2
-3
-4
-5
-6
-7
-8
-9
-0
-
-
-
-f
-i
-g
-h
-t
- 
-m
-e
-
+#include <stdio.h>
+#include <iostream>
+int main() {
+std::cout << "yeet\nyou know what\nkiran\nfight\n me \ni \nsee\nyou\ntyping\n1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n\n\n\nf\ni\ng\nh\nt\n\nm\ne" << std::endl;
+}
